1ac45fa59e63aaf1e8a384a625b7c87171ba3a3e
justinmundie <100366197+justinmundie@users.noreply.github.com>
2022-02-28 14:10:22 -0500

    Finalised

--- main.ts
+++ main.ts
@@ -1,36 +1,47 @@
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
-        # # # # #
+        . # # # .
         # . . . .
-        # # # # #
-        . . . . #
-        # # # # #
+        # # # # .
+        . . . # .
+        # # # . .
         `)
     basic.pause(500)
-    basic.showNumber(Strikes)
     Strikes += 1
+    basic.showNumber(Strikes)
     if (Strikes == 3) {
+        basic.pause(500)
+        basic.showString("Strike out")
         Strikes = 0
         Balls = 0
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Reset")
+    Strikes = 0
+    Balls = 0
+})
 input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # . .
+        # . . # .
+        # . # . .
+        # . . # .
+        # # # . .
+        `)
+    basic.pause(500)
+    Balls += 1
+    basic.showNumber(Balls)
     if (Balls == 4) {
+        basic.pause(500)
+        basic.showString("Walk")
         Strikes = 0
         Balls = 0
     }
-    basic.showNumber(Balls)
-    Balls += 1
 })
 let Balls = 0
 let Strikes = 0
-basic.showLeds(`
-    . # # # .
-    # . # . #
-    # # # # #
-    # . # . #
-    . # # # .
-    `)
+basic.showString("GAME!")
 basic.clearScreen()
 Strikes = 0
 Balls = 0
